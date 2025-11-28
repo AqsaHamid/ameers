b5953ec1960bfa10a0fb4f9b04dbfb38fe665b1b
AqsaHamid <aqsahamid4020730@gmail.com>
2025-11-28 11:59:00 +0500

    header strcuture

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Footer from "./assets/components/common/Footer";
+import Header from "./assets/components/common/Header";
 function App() {
     return (
+        <>
+        <Header />
         <Footer />
+        </>
+      
     )
 }
 export default App;
---
+++ src/assets/components/common/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="w-full h-4">
+      <div className="w-20 h-20">
+        <img src="/logo.jpeg" alt="" />
+      </div>
+    </header>
+  );
+};
+export default Header;
